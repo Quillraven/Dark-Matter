f018741c6af838633c5fb28d31e1e1cdcaf36244
Quillraven <quillraven@gmail.com>
2020-04-12 21:42:27 +0200

    remove box2d

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -6,7 +6,6 @@ object Versions {
     const val agp = "3.6.0"
     const val gdx = "1.9.11-SNAPSHOT"
     const val ktx = "1.9.10-b5"
-    const val box2dLight = "1.5"
     const val ashley = "1.7.3"
     val java = JavaVersion.VERSION_1_8
 }
