cde878a91cafbede683f449fcd25c773f098dd27
Quillraven <quillraven@gmail.com>
2020-04-14 18:21:50 +0200

    add debug method to kill player (NUM_1)

--- core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/debug.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/debug.kt
@@ -3,6 +3,7 @@ package com.github.quillraven.darkmatter.ecs.system
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IntervalIteratingSystem
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.github.quillraven.darkmatter.ecs.component.PlayerComponent
 import com.github.quillraven.darkmatter.ecs.component.TransformComponent
 import ktx.ashley.allOf
@@ -12,6 +13,13 @@ class DebugSystem : IntervalIteratingSystem(allOf(PlayerComponent::class).get(),
     override fun processEntity(entity: Entity) {
         entity[PlayerComponent.mapper]?.let { player ->
             entity[TransformComponent.mapper]?.let { transform ->
+                if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)) {
+                    // kill player
+                    transform.position.y = 1f
+                    player.life = 1f
+                    player.shield = 0f
+                }
+
                 Gdx.graphics.setTitle("Dark Matter Debug - pos:${transform.position}, life:${player.life}, shield:${player.shield}")
             }
         }
