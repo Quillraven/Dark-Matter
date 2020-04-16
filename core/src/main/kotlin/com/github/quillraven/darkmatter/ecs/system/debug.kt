7515f9a0555e7621f6b5a4a9b30d46bcfd2ffa55
Quillraven <quillraven@gmail.com>
2020-04-16 21:45:05 +0200

    add debug method to heal the player (NUM_7)

--- core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/debug.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/debug.kt
@@ -5,11 +5,14 @@ import com.badlogic.ashley.systems.IntervalIteratingSystem
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.github.quillraven.darkmatter.ecs.component.PlayerComponent
+import com.github.quillraven.darkmatter.ecs.component.PowerUpType
 import com.github.quillraven.darkmatter.ecs.component.TransformComponent
 import com.github.quillraven.darkmatter.event.GameEventManager
+import com.github.quillraven.darkmatter.event.GameEventPlayerDamaged
 import com.github.quillraven.darkmatter.event.GameEventType
 import ktx.ashley.allOf
 import ktx.ashley.get
+import kotlin.math.max
 
 class DebugSystem(
     private val gameEventManager: GameEventManager
@@ -25,19 +28,32 @@ class DebugSystem(
                         player.shield = 0f
                     }
                     Gdx.input.isKeyPressed(Input.Keys.NUM_2) -> {
+                        // set shield
                         player.shield = 100f
                     }
                     Gdx.input.isKeyPressed(Input.Keys.NUM_3) -> {
+                        // remove shield
                         player.shield = 0f
                     }
                     Gdx.input.isKeyPressed(Input.Keys.NUM_4) -> {
+                        // disable vertical movement
                         engine.getSystem(VerticalMoveSystem::class.java).setProcessing(false)
                     }
                     Gdx.input.isKeyPressed(Input.Keys.NUM_5) -> {
+                        // enable vertical movement
                         engine.getSystem(VerticalMoveSystem::class.java).setProcessing(true)
                     }
                     Gdx.input.isKeyPressed(Input.Keys.NUM_6) -> {
-                        gameEventManager.dispatchEvent(GameEventType.PLAYER_DAMAGED)
+                        // trigger player damage event
+                        player.life = max(1f, player.life - 5f)
+                        gameEventManager.dispatchEvent(GameEventType.PLAYER_DAMAGED, GameEventPlayerDamaged.apply {
+                            this.player = entity
+                        })
+                    }
+                    Gdx.input.isKeyPressed(Input.Keys.NUM_7) -> {
+                        // trigger player heal event
+                        engine.getSystem(PowerUpSystem::class.java)
+                            .spawnPowerUp(PowerUpType.LIFE, transform.position.x, transform.position.y)
                     }
                 }
 
