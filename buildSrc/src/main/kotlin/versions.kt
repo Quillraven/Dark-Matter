1e852ac6718125d874293b901ec8e14fb5e7ce8a
Quillraven <quillraven@gmail.com>
2020-04-17 19:03:27 +0200

    switch to ktx SNAPSHOT

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val coroutines = "1.3.5"
     const val agp = "3.6.0"
     const val gdx = "1.9.11-SNAPSHOT"
-    const val ktx = "1.9.10-b5"
+    const val ktx = "1.9.10-SNAPSHOT"
     const val ashley = "1.7.3"
     val java = JavaVersion.VERSION_1_8
 }
