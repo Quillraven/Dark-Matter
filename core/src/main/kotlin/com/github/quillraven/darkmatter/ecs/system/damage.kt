521e0896ba835d2ce5914eef055b5ae40e5dd18a
Quillraven <quillraven@gmail.com>
2020-04-18 20:13:28 +0200

    add damage and block sound effects

--- core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/damage.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/damage.kt
@@ -2,6 +2,8 @@ package com.github.quillraven.darkmatter.ecs.system
 
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
+import com.github.quillraven.darkmatter.asset.SoundAsset
+import com.github.quillraven.darkmatter.audio.AudioService
 import com.github.quillraven.darkmatter.ecs.component.AnimationComponent
 import com.github.quillraven.darkmatter.ecs.component.AnimationType
 import com.github.quillraven.darkmatter.ecs.component.GraphicComponent
@@ -18,11 +20,41 @@ import ktx.ashley.get
 import kotlin.math.max
 
 private const val DAMAGE_PER_SECOND = 25f
+private const val DAM_SOUND_LENGTH = 0.6f
+private const val BLOCK_SOUND_LENGTH = 0.6f
 
 class DamageSystem(
-    private val gameEventManager: GameEventManager
+    private val gameEventManager: GameEventManager,
+    private val audioService: AudioService
 ) :
     IteratingSystem(allOf(PlayerComponent::class, TransformComponent::class).exclude(RemoveComponent::class).get()) {
+    private var damageTaken = false
+    private var blocked = false
+    private var damageSoundCountdown = 0f
+    private var blockSoundCountdown = 0f
+
+    override fun update(deltaTime: Float) {
+        damageTaken = false
+        blocked = false
+        super.update(deltaTime)
+        if (blocked) {
+            if (blockSoundCountdown <= 0f) {
+                audioService.play(SoundAsset.BLOCK)
+                blockSoundCountdown = BLOCK_SOUND_LENGTH
+            } else {
+                blockSoundCountdown -= deltaTime
+            }
+        }
+        if (damageTaken) {
+            if (damageSoundCountdown <= 0f) {
+                audioService.play(SoundAsset.DAMAGE)
+                damageSoundCountdown = DAM_SOUND_LENGTH
+            } else {
+                damageSoundCountdown -= deltaTime
+            }
+        }
+    }
+
     override fun processEntity(entity: Entity, deltaTime: Float) {
         entity[TransformComponent.mapper]?.let { transform ->
             entity[PlayerComponent.mapper]?.let { player ->
@@ -30,6 +62,7 @@ class DamageSystem(
                     var damage = DAMAGE_PER_SECOND * deltaTime
                     if (player.shield > 0f) {
                         val blockAmount = player.shield
+                        blocked = true
                         player.shield = max(player.shield - damage, 0f)
                         damage -= blockAmount
                         if (damage <= 0f) {
@@ -38,6 +71,7 @@ class DamageSystem(
                         }
                     }
 
+                    damageTaken = true
                     player.life -= damage
 
                     gameEventManager.dispatchEvent(GameEventType.PLAYER_DAMAGED, GameEventPlayerDamaged.apply {
@@ -51,7 +85,7 @@ class DamageSystem(
                         engine.entity {
                             with<TransformComponent> {
                                 size.set(1.5f, 1.5f)
-                                position.set(transform.position).z = 2f
+                                setInitialPosition(transform.position.x, transform.position.y, 2f)
                             }
                             with<AnimationComponent> {
                                 type = AnimationType.EXPLOSION
@@ -61,6 +95,7 @@ class DamageSystem(
                                 delay = 0.9f
                             }
                         }
+                        audioService.play(SoundAsset.EXPLOSION)
                     }
                 }
             }
