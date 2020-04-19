32c287e1eb455f63289af6f9787884676b5fab3e
Quillraven <quillraven@gmail.com>
2020-04-19 16:24:58 +0200

    add ktx scene2d related things

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,12 +9,15 @@ dependencies {
     implementation("com.badlogicgames.gdx:gdx:${Versions.gdx}")
     implementation("com.badlogicgames.ashley:ashley:${Versions.ashley}")
     api("io.github.libktx:ktx-app:${Versions.ktx}")
+    implementation("io.github.libktx:ktx-actors:${Versions.ktx}")
     implementation("io.github.libktx:ktx-ashley:${Versions.ktx}")
     implementation("io.github.libktx:ktx-assets-async:${Versions.ktx}")
     implementation("io.github.libktx:ktx-collections:${Versions.ktx}")
     implementation("io.github.libktx:ktx-math:${Versions.ktx}")
     implementation("io.github.libktx:ktx-graphics:${Versions.ktx}")
     implementation("io.github.libktx:ktx-log:${Versions.ktx}")
+    implementation("io.github.libktx:ktx-scene2d:${Versions.ktx}")
+    implementation("io.github.libktx:ktx-style:${Versions.ktx}")
 }
 
 java {
