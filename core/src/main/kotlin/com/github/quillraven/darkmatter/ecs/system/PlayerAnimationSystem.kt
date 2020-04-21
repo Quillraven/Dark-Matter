55bd3c8a97bd58c24c8cf93c2dfff8f3bbe8ee4c
Quillraven <quillraven@gmail.com>
2020-04-21 21:37:17 +0200

    set player sprite region when added to engine

--- core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/PlayerAnimationSystem.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/ecs/system/PlayerAnimationSystem.kt
@@ -1,6 +1,8 @@
 package com.github.quillraven.darkmatter.ecs.system
 
+import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.EntityListener
 import com.badlogic.ashley.systems.IteratingSystem
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.github.quillraven.darkmatter.ecs.component.FacingComponent
@@ -14,8 +16,19 @@ class PlayerAnimationSystem(
     private val defaultRegion: TextureRegion,
     private val leftRegion: TextureRegion,
     private val rightRegion: TextureRegion
-) : IteratingSystem(allOf(PlayerComponent::class, FacingComponent::class, GraphicComponent::class).get()) {
+) : IteratingSystem(allOf(PlayerComponent::class, FacingComponent::class, GraphicComponent::class).get()),
+    EntityListener {
     private var lastDirection = FacingDirection.DEFAULT
+
+    override fun addedToEngine(engine: Engine) {
+        super.addedToEngine(engine)
+        engine.addEntityListener(family, this)
+    }
+
+    override fun removedFromEngine(engine: Engine) {
+        super.removedFromEngine(engine)
+        engine.removeEntityListener(this)
+    }
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
         val facing = entity[FacingComponent.mapper]
@@ -36,4 +49,10 @@ class PlayerAnimationSystem(
         }
         graphic.setSpriteRegion(region)
     }
+
+    override fun entityAdded(entity: Entity) {
+        entity[GraphicComponent.mapper]?.setSpriteRegion(defaultRegion)
+    }
+
+    override fun entityRemoved(entity: Entity?) = Unit
 }
