f3eed829dcb71201e7377b95e994a175212d6db4
Quillraven <quillraven@gmail.com>
2020-04-21 21:36:18 +0200

    add ImageButtonStyle for quit, pause and resume button

--- core/src/main/kotlin/com/github/quillraven/darkmatter/ui/skin.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/ui/skin.kt
@@ -7,37 +7,58 @@ import com.github.quillraven.darkmatter.asset.TextureAtlasAsset
 import ktx.assets.async.AssetStorage
 import ktx.scene2d.Scene2DSkin
 import ktx.style.SkinDsl
+import ktx.style.imageButton
 import ktx.style.label
 import ktx.style.skin
 
-enum class LabelStyle {
+enum class SkinLabel {
     LARGE, DEFAULT
 }
 
-enum class UIImage(val atlasKey: String) {
+enum class SkinImageButton {
+    PAUSE_PLAY, QUIT
+}
+
+enum class SkinImage(val atlasKey: String) {
     GAME_HUD("game_hud"),
     WARNING("warning"),
     LIFE_BAR("life_bar"),
-    SHIELD_BAR("shield_bar")
+    SHIELD_BAR("shield_bar"),
+    PLAY("play"),
+    PAUSE("pause"),
+    QUIT("quit")
 }
 
 fun createSkin(assets: AssetStorage) {
     val atlas = assets[TextureAtlasAsset.UI.descriptor]
     val bigFont = assets[BitmapFontAsset.FONT_LARGE_GRADIENT.descriptor]
     val defaultFont = assets[BitmapFontAsset.FONT_DEFAULT.descriptor]
-    Scene2DSkin.defaultSkin = skin(atlas) {
-        createLabels(bigFont, defaultFont)
+    Scene2DSkin.defaultSkin = skin(atlas) { skin ->
+        createLabelStyles(bigFont, defaultFont)
+        createImageButtonStyles(skin)
     }
 }
 
-private fun @SkinDsl Skin.createLabels(
+private fun @SkinDsl Skin.createImageButtonStyles(skin: Skin) {
+    imageButton(SkinImageButton.PAUSE_PLAY.name) {
+        imageUp = skin.getDrawable(SkinImage.PAUSE.atlasKey)
+        imageChecked = skin.getDrawable(SkinImage.PLAY.atlasKey)
+        imageDown = imageChecked
+    }
+    imageButton(SkinImageButton.QUIT.name) {
+        imageDown = skin.getDrawable(SkinImage.QUIT.atlasKey)
+        imageUp = imageDown
+    }
+}
+
+private fun @SkinDsl Skin.createLabelStyles(
     bigFont: BitmapFont,
     defaultFont: BitmapFont
 ) {
-    label(LabelStyle.LARGE.name) {
+    label(SkinLabel.LARGE.name) {
         font = bigFont
     }
-    label(LabelStyle.DEFAULT.name) {
+    label(SkinLabel.DEFAULT.name) {
         font = defaultFont
     }
 }
