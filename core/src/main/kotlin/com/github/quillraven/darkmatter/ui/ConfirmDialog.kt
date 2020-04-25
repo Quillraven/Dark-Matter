3437ce1d785533c238a09b2e921d37a83ee46691
Quillraven <quillraven@gmail.com>
2020-04-25 18:51:37 +0200

    add correct padding to textbutton labels and replace with ktx scene2d extensions

--- core/src/main/kotlin/com/github/quillraven/darkmatter/ui/ConfirmDialog.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/ui/ConfirmDialog.kt
@@ -1,25 +1,30 @@
 package com.github.quillraven.darkmatter.ui
 
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.I18NBundle
 import com.github.quillraven.darkmatter.V_HEIGHT_PIXELS
 import com.github.quillraven.darkmatter.V_WIDTH_PIXELS
 import ktx.scene2d.Scene2DSkin
+import ktx.scene2d.label
+import ktx.scene2d.scene2d
+import ktx.scene2d.textButton
 
 private const val ELEMENT_PADDING = 7f
 private const val DIALOG_WIDTH_SCALE = 0.75f
 private const val DIALOG_HEIGHT_SCALE = 0.5f
 
 class ConfirmDialog(bundle: I18NBundle) : Dialog("", Scene2DSkin.defaultSkin, SkinWindow.DEFAULT.name) {
-    val yesButton = TextButton(" ${bundle["yes"]} ", Scene2DSkin.defaultSkin, SkinTextButton.DEFAULT.name)
-    val noButton = TextButton(" ${bundle["no"]} ", Scene2DSkin.defaultSkin, SkinTextButton.DEFAULT.name)
+    val yesButton = scene2d.textButton(bundle["yes"], SkinTextButton.DEFAULT.name) {
+        labelCell.padLeft(ELEMENT_PADDING).padRight(ELEMENT_PADDING)
+    }
+    val noButton = scene2d.textButton(bundle["no"], SkinTextButton.DEFAULT.name) {
+        labelCell.padLeft(ELEMENT_PADDING).padRight(ELEMENT_PADDING)
+    }
 
     init {
         contentTable.defaults().fillX().expandX().pad(ELEMENT_PADDING)
-        text(Label(bundle["areYouSure"], Scene2DSkin.defaultSkin, SkinLabel.DEFAULT.name).apply {
+        text(scene2d.label(bundle["areYouSure"], SkinLabel.DEFAULT.name).apply {
             wrap = true
             setAlignment(Align.center)
         })
