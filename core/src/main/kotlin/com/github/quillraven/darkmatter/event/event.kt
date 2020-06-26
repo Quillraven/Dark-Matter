f4d335730e3a41479fcbad7143ada89d473ae4f1
Quillraven <quillraven@gmail.com>
2020-06-26 10:39:48 +0200

    add toString function to event objects

--- core/src/main/kotlin/com/github/quillraven/darkmatter/event/event.kt
+++ core/src/main/kotlin/com/github/quillraven/darkmatter/event/event.kt
@@ -16,27 +16,37 @@ sealed class GameEvent {
     object PlayerSpawn : GameEvent()
     object PlayerDeath : GameEvent() {
         var distance = 0f
+
+        override fun toString() = "PlayerDeath(distance=$distance)"
     }
 
     object PlayerBlock : GameEvent() {
         var shield = 0f
         var maxShield = 0f
+
+        override fun toString() = "PlayerBlock(shield=$shield,maxShield=$maxShield)"
     }
 
     object PlayerMove : GameEvent() {
         var distance = 0f
         var speed = 0f
+
+        override fun toString() = "PlayerMove(distance=$distance,speed=$speed)"
     }
 
     object PlayerHit : GameEvent() {
         lateinit var player: Entity
         var life = 0f
         var maxLife = 0f
+
+        override fun toString() = "PlayerHit(player=$player,life=$life,maxLife=$maxLife)"
     }
 
     object PowerUp : GameEvent() {
         lateinit var player: Entity
         var type = PowerUpType.NONE
+
+        override fun toString() = "PowerUp(player=$player,type=$type)"
     }
 }
 
