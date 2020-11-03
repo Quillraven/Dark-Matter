224ec6ffece3e275af0cddd1cb89c53c97290504
Quillraven <quillraven@gmail.com>
2020-11-03 21:26:15 +0100

    update to libgdx 1.9.12-SNAPSHOT to fix glyphlayout issues

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val kotlin = "1.4.10"
     const val coroutines = "1.3.9"
     const val agp = "3.6.0"
-    const val gdx = "1.9.11"
+    const val gdx = "1.9.12-SNAPSHOT"
     const val ktx = "1.9.11-b2"
     const val ashley = "1.7.3"
     const val detekt = "1.7.4"
